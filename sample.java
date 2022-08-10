db65e00218b344225151661217b66b473ec9f313
Praghatha <110326452+Praghatha@users.noreply.github.com>
2022-08-10 10:45:27 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,7 @@
+public class sample
+{
+  public static void main(String[] args)
+  {
+    System.out.println("hai good morning");
+    System.out.println("how are you");
+    System.out.println("i am fine");
